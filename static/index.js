5b154191f5d6af586da49c9c1f5c10c7cfe7b99f
David-Yan-22 <73031516+David-Yan-22@users.noreply.github.com>
2023-10-25 20:42:12 +1100

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,3 +1,12 @@
+// Define constants
+const HOME = "home";
+const PROJECTS = "projects";
+const ARTICLES = "articles";
+const ABOUT_ME = "about-me";
+const QUESTION_AND_ANSWER = "question-answer";
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     load_page('home');
 });
